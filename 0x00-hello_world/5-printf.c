54f069ad9b7f0be70fa02cdf8583a9758ce11a99
Akumobi Kingsley <akumobi@gmail.com>
2023-03-16 10:39:16 -0700

    This is a C program that prints exactly 'with proper grammar, but the outcome is a piece of art,' followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program begins here
+ * Return: Always zero (0) if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
