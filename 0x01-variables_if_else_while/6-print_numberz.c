ea43faddace50f18ce584112d855b7a23a573379
Akumobi Kingsley <akumobi@gmail.com>
2023-03-17 20:47:46 -0700

    PUTTING more numbers!

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program starts here
+ * Return: program returns zero (0) when it runs successfully
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 48; k < 58; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
