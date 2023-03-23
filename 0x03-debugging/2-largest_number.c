4e02c11a2aad91cd7ff5506a96452530b90856c1
Akumobi Kingsley <akumobi@gmail.com>
2023-03-23 03:27:20 -0700

    Largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (c > b)
-	{
-		largest = c;
-	}
-	else
+	else if (b > c)
 	{
 		largest = b;
 	}
+	else
+	{
+		largest = c;
+	}
+
 	return (largest);
 }
