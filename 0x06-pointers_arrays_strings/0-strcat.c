bfe60c5ec9ee85e833ff9a2c73cf007a76829486
Akumobi Kingsley <akumobi@gmail.com>
2023-03-29 07:40:41 -0700

    This function concatenates 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: value of destination string
+ * @src: value of source string
+ * Return: void is returned
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int agbado;
+	int blu;
+
+	agbado = 0;
+	while (dest[agbado] != '\0')
+	{
+		agbado++;
+	}
+	blu = 0;
+	while (src[blu] != '\0')
+	{
+		dest[agbado] = src[blu];
+		agbdao++;
+		blu++;
+	}
+
+	dest[agbado] = '\0';
+	return (dest);
+}
