bd5d6dd20243fd974acd7ff07c461a4b5414224c
Akumobi Kingsley <akumobi@gmail.com>
2023-04-08 22:23:17 -0700

    Print Program Name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Function of the Program
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Zero for success
+ */
+
+int main (int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", *argv);
+	return (0);
+}
